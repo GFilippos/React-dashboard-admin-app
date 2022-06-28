1d4879836c30c196784fa7353229a1566316d63a
Filippos Gomelis <filippos.gomelis@aqurance.local>
2022-06-28 09:15:38 +0300

    home-page-ready

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
+            {/* My Routes */}
             <div>
               <Routes>
                 {/* Dashboard */}
